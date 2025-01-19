72ea6cd7d566fcd3c5b53ae75d3466c19dbcf966
辉夜 <rx700@vip.qq.com>
2025-01-19 19:43:45 +0800

    Delete CrypticLauncherBeautify directory

--- CrypticLauncherBeautify/Theme/Theme.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CrypticLauncherBeautify.Theme;
-
-public class Theme
-{
-    public string ThemeName { get; set; } = string.Empty;
-    public EngagePage? EngagePage { get; set; }
-    public LoginPage? LoginPage { get; set; }
-}
